9d951db2ad29895c8b75fd421bdc6022ed9d001c
David Silva Troya <davidsiltroy2008@hotmail.com>
2023-01-17 13:55:19 +0100

    Now the detect from detect.py is working as a functions with parameters and not from arguments in the command line

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import numpy as np
 import cv2
 import io
 from flask_restful import reqparse, abort, Api, Resource
+import strw_detect
+
+import json
+
+
 
 app = Flask("StrawberryFlowersAPI")
 api = Api(app)
@@ -12,7 +17,7 @@ api = Api(app)
 # route http posts to this method
 @app.route('/api/test', methods=['POST'])
 def test():
-    
+
     print('Checking picture')
     # Get the image data from the POST request
     image_data = request.files['image'].read()
@@ -28,6 +33,24 @@ def test():
     response = app.make_response(buffer.tobytes())
     response.headers.set('Content-Type', 'image/jpeg')
     return response
+
+@app.route('/detect-objects', methods=['POST'])
+def detect_objects():
+    # Get the image data from the POST request
+    image_data = request.files['image'].read()
+    # Decode image data as a numpy array
+    image = np.frombuffer(image_data, np.uint8)
+    # Decode image as a color image
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    # Run the object detector
+    # objects =  
+
+    strw_detect.strw_detect(source='fortest', weights=['rtrain-2.pt'],conf_thres= 0.5, img_size=640,name="fromapi")
+    # Create a response with the object detections
+    # response = app.make_response(json.dumps(objects))
+    # response.headers.set('Content-Type', 'application/json')
+    # return response
+    return 'haciendo xd'
 
 
 if __name__ == '__main__':
