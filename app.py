d5cf09ca6aeeef02006581695b3077fd50f9e883
David Silva Troya <davidsiltroy2008@hotmail.com>
2023-01-19 14:31:50 +0100

    Ready to check the libraries from YOLOv7 that we actually need

--- app.py
+++ app.py
@@ -18,20 +18,23 @@ def apindex():
 @app.route('/api/test', methods=['POST'])
 def test():
     print('Checking picture')
-    # Get the image data from the POST request
-    image_data = request.files['image'].read()
-    # Decode image data as a numpy array
-    image = np.frombuffer(image_data, np.uint8)
-    # Decode image as a color image
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    # Process the image (e.g. resize, crop, etc.)
-    processed_image = cv2.resize(image, (800, 600))
-    # Encode processed image to jpeg format
-    ret, buffer = cv2.imencode('.jpg', processed_image)
-    # Create a response with the processed image
-    response = app.make_response(buffer.tobytes())
-    response.headers.set('Content-Type', 'image/jpeg')
-    return response
+    if request.files['image']:
+        # Get the image data from the POST request
+        image_data = request.files['image'].read()
+        # Decode image data as a numpy array
+        image = np.frombuffer(image_data, np.uint8)
+        # Decode image as a color image
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        # Process the image (e.g. resize, crop, etc.)
+        processed_image = cv2.resize(image, (800, 600))
+        # Encode processed image to jpeg format
+        ret, buffer = cv2.imencode('.jpg', processed_image)
+        # Create a response with the processed image
+        response = app.make_response(buffer.tobytes())
+        response.headers.set('Content-Type', 'image/jpeg')
+        return response
+    else:
+        return "nothing"
 
 if __name__ == '__main__':
     app.run()
