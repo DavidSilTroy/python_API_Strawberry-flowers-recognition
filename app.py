87c189fa59613588a2d4633c9f9618e28c1e0a86
David Silva Troya <davidsiltroy2008@hotmail.com>
2023-01-19 13:03:18 +0100

    All Simple

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-import base64
 from flask import Flask, request, Response, send_file, jsonify
-from PIL import Image
-import jsonpickle
-import numpy as np
-import cv2
-import io
-
 
 
 app = Flask("StrawberryFlowersAPI")
@@ -18,9 +11,6 @@ def index():
 @app.route('/api')
 def apindex():
     return "Hello world from API for strwberry detection"
-
-
-    
 
 if __name__ == '__main__':
     app.run()
