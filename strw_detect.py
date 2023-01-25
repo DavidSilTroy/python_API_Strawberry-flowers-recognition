5949f44a15bcf65c6b446f3db9bffa4afaaed7c0
David Silva Troya <davidsiltroy2008@hotmail.com>
2023-01-25 08:53:15 +0100

    Removing some not usuful files

--- strw_detect.py
+++ strw_detect.py
@@ -2,7 +2,7 @@ import time
 
 import cv2
 import torch
-import torch.backends.cudnn as cudnn
+# import torch.backends.cudnn as cudnn
 import numpy as np
 
 from models.experimental import attempt_load
