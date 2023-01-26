3e82380d894ba5c9c3747679b9054c87b1255d9b
David Silva Troya <davidsiltroy2008@hotmail.com>
2023-01-26 20:54:32 +0100

    Improving the API, now it has CORS and a Error handler for bad requests

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import base64
 from flask import Flask, request, Response, send_file, jsonify
 import io
 import strw_detect
+import werkzeug
+from flask_cors import CORS
 
 
 y7_stw = strw_detect.StrwbDetection()
 app = Flask("StrawberryFlowersAPI")
+CORS(app)
 
 @app.route('/')
 def index():
@@ -54,15 +57,20 @@ def get_image_from_object_detection():
         # Return the image as response
         return send_file(io.BytesIO(img_bytes), mimetype='image/jpeg')
     else:
-        return "problem detecting objects.."
+        response = {
+            'image': "Problem with the request",
+            'result': request.get_json()['image']
+        }
+        # Return the response as JSON
+        return jsonify(response)
+        # return "problem detecting objects.."
 
 
 
 @app.route('/api/data-strawberry', methods=['POST'])
 def get_data_from_object_detection():
     image = 0
-    if request.is_json: 
-        # Get the image data from the request body
+    if request.is_json:         # Get the image data from the request body
         image_data = request.get_json()['image']
         # Decode base64 image to bytes
         image_bytes = base64.b64decode(image_data)
@@ -77,19 +85,44 @@ def get_data_from_object_detection():
     if isinstance(image, bytes):
         #object detection
         img_detected, result = y7_stw.detect_strw_flowers(image)
-        # Convert the image to bytes
-        img_bytes = img_detected.tobytes()
-        # Convert the image bytes to a base64 encoded string
-        img_base64 = base64.b64encode(img_bytes).decode()
-        # Create a dictionary to hold the image and result
+        try:
+            # Convert the image to bytes
+            img_bytes = img_detected.tobytes()
+            # Convert the image bytes to a base64 encoded string
+            img_base64 = base64.b64encode(img_bytes).decode()
+            #Removing the ,
+            if result.count(','):
+                result = result.replace(',',', ')
+                result = result[:-2]
+            # Create a dictionary to hold the image and result
+            response = {
+                'image': img_base64,
+                'result': result
+            }
+            # Return the response as JSON
+            return jsonify(response)
+        except:
+            response = {
+                'image': "Null",
+                'result': result
+            }
+            # Return the response as JSON
+            return jsonify(response)
+
+    else:
         response = {
-            'image': img_base64,
-            'result': result
+            'image': "Problem with the request",
+            'result': request.get_json()['image']
         }
         # Return the response as JSON
         return jsonify(response)
-    else:
-        return "problem detecting objects.."
+        # return "problem detecting objects.."
+
+
+@app.errorhandler(werkzeug.exceptions.BadRequest)
+def handle_bad_request(e):
+    print(f'bad request! {e} \n\n\n', 400)
+    return f'bad request! {e} \n\n\n', 400
 
 
 
